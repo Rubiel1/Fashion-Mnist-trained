8e18f4d86dd704d434a4cf9ec85fde713df5ab1c
Eric Rubiel <eric.rubiel@u.northwestern.edu>
2018-10-02 10:58:49 -0400

    uploaded diagram

--- fashion.py
+++ fashion.py
@@ -14,6 +14,8 @@ from keras import regularizers
 from keras.callbacks import EarlyStopping
 import numpy as np
 from keras.datasets import fashion_mnist
+import gc
+
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
 X_train = X_train.astype('float32') / 255
 X_train = X_train.reshape(X_train.shape[0], 28, 28,1)
@@ -31,12 +33,12 @@ image_input = Input(shape = (28,28,1))
 
 branch_a = layers.Conv2D(64, 3, activation='relu')(image_input)
 branch_a = layers.Conv2D(128,4, strides = 2, activation='relu' )(branch_a)
-branch_a  = layers.BatchNormalization(axis = -1)(branch_a)
+
 branch_b = layers.Conv2D(64, 2, strides = 2, activation='relu')(image_input)
 branch_b = layers.Conv2D(128,3, activation = 'relu')(branch_b)
-branch_b = layers.BatchNormalization(axis = -1)(branch_b)
 
 output1 = layers.concatenate([branch_a, branch_b], axis = -1)
+output1 = layers.BatchNormalization(axis = -1)(output1)
 
 branch_ab = layers.Conv2D(128, 5, padding = 'same', activation = 'relu', kernel_regularizer = regularizers.l1(.001) )(output1)
 branch_c = layers.Conv2D(64,7, padding = 'same', activation='relu')(image_input)
@@ -56,8 +58,12 @@ model = Model(image_input, predictions)
 model.summary()
 
 model.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['acc'])
-model.fit(X_train, y_train, batch_size =300, epochs = 50, callbacks = callbacks_list)
+model.fit(X_train, y_train, batch_size =500, epochs = 20, callbacks = callbacks_list)
 loss, acc = model.evaluate (X_test, y_test)
 print('final accuracy = ', acc)
 model.save('fashion.h5')
+
 print('model saved')
+from keras.utils import plot_model
+plot_model(model, to_file='fashion.png')
+gc.collect()
